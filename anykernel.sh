a869ac18385d336bf6e68756aed194dcb3ef4b25
RJTangpos <rjtangpos39@gmail.com>
2023-10-16 01:15:52 +0800

    Update

--- anykernel.sh
+++ anykernel.sh
@@ -2,35 +2,42 @@
 ## osm0sis @ xda-developers
 
 ### AnyKernel setup
-# begin properties
+# global properties
 properties() { '
-kernel.string=DarkPhantom-KSU kernel by @RJ_D3900 
+kernel.string=DarkPhantom-KSU kernel by @RJ_D3900
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
-do.cleanuponabort=1
+do.cleanuponabort=0
 device.name1=X2
 device.name2=RMX1991
 device.name3=RMX1993
 device.name2=RMX1992EX
 supported.versions=
 supported.patchlevels=
+supported.vendorpatchlevels=
 '; } # end properties
 
-## boot shell variables
+### AnyKernel install
+# boot shell variables
 block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=0;
+is_slot_device=auto;
 ramdisk_compression=auto;
-no_block_display=true;
 patch_vbmeta_flag=auto;
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
 
-## AnyKernel install
+## boot files attributes
+boot_attributes() {
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+} # end attributes
+
+## AnyKernel boot install
 split_boot;
+
 flash_boot;
 flash_dtbo;
-## end install
-
+## end boot install
